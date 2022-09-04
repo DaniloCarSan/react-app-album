71e756cb67db6510c11819689274ec7fe77d24e0
Danilo Santos <danilocarsan@gmail.com>
2022-09-04 20:25:59 -0300

    organizando componentes

---
+++ src/ui/App.styles.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    padding: 60px;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    height: 60px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const Title = styled.h1`
+    text-transform: uppercase;
+`;
+
+export const Content = styled.div`
+    margin-top: 30px;
+    display: grid;
+    background-color: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
--- src/ui/pages/Home/styles.tsx
+++ src/ui/pages/Home/styles.tsx
@@ -1,30 +1,5 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    padding: 60px;
-    height: 100vh;
-`;
-
-export const Header = styled.header`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: white;
-    height: 60px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-`;
-
-export const Title = styled.h1`
-    text-transform: uppercase;
-`;
-
-export const Content = styled.div`
-    margin-top: 30px;
-    display: grid;
-    background-color: white;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-`;
-
 export const Album = styled.div`
     padding: 25px;
     border-bottom: 1px solid #eee;
